da46a91b5512af88aa8ad4f15f392485959924a4
Abdul Jaseem VP <67306692+vpjaseem@users.noreply.github.com>
2025-10-23 19:22:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
                             <div class="step-icon">
                                 <img src="img/stackWhite.svg">
                             </div>
-                            <div class="step-text">Connect me on <a href="https://www.linkedin.com/in/abdul-jaseem-ab1560114/">LinkedIn</a> | <a href="https://github.com/vpjaseem">GitHub</a></div>
+                            <div class="step-text"><a href="https://aj-labs.com/" target="_blank">Connect me</a> </div>
                         </div>
                         <div class="step">
                             <div class="step-icon">
